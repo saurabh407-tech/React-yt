3986b78199e4365e5bff2227732ba9bb53b39e9a
saurabh407-tech <sourabhshukla8318@gmail.in>
2026-01-14 00:04:18 +0530

    Initial React project commit

---
+++ 03-components/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Card from './components/card';
+
+const App = () => {
+  const user='Saurabh Shukla';
+  return (
+    <div>
+      <h1>Hello Guys Myself {user}</h1>   {/*bracket i wajah se variable ka value aa jata hai*/}
+      <div className='card'>
+        <h1>Saurabh Shukla</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+      </div>
+       <Card />
+    </div>
+  )
+}
+
+export default App
---
+++ 03-components/src/components/card.jsx
@@ -0,0 +1,8 @@
+function Card(){
+    return <div className='card'>
+        <h1>Sachin Shukla</h1>
+        <p>The king of uttar pradesh.</p>
+      </div>
+}
+
+export default Card;
