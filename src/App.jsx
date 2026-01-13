203cc384c9ce5e0e2287aa6c8c2302824d2e33c5
saurabh407-tech <sourabhshukla8318@gmail.in>
2026-01-13 16:50:31 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+     <div id='parent'>
+      <h1 id='child1'>Saurabh</h1>
+      <h2 id='child2'>Shukla</h2>
+      <div id='chacha'>
+        <h5 id='bhai1'>Raja</h5>
+        <h6 id='bhai2'>Atul</h6>
+      </div>
+     </div>
+    </div>
+  )
+}
+
+export default App
