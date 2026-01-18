49ae3c25df80c4df76d0c28459f4f6ed81e15ca7
saurabh407-tech <sourabhshukla8318@gmail.in>
2026-01-18 19:55:04 +0530

    fourth

---
+++ 06-CSS/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Button from './components/button/Button.jsx'
+import Header from './components/header/Header.jsx'
+
+const App = () => {
+  return (
+    <div>
+      <Button />
+      <Header />
+    </div>
+  )
+}
+
+export default App
